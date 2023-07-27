5a8283a9675805ceca0929113ec00139bc5c5f76
Kanza Syed <36698316+kanzasyed@users.noreply.github.com>
2023-07-27 14:06:01 +0000

    handling CRUD Operations

--- forms.py
+++ forms.py
@@ -4,4 +4,7 @@ from wtforms.validators import DataRequired
 
 class AddTaskForm(FlaskForm):
     title=StringField('Title',validators=[DataRequired()])
-    submit=SubmitField('submit')
+    submit=SubmitField('Submit')
+
+class DeleteTaskForm(FlaskForm):
+    submit=SubmitField('Delete')    
